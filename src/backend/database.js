128817950b968052e85081f8110cc2f7b09f5310
priyankapandey13 <55982058+priyankapandey13@users.noreply.github.com>
2020-04-17 14:08:43 +0200

    Update database.js

--- src/backend/database.js
+++ src/backend/database.js
@@ -1,27 +1,11 @@
 const mysql = require('mysql')
-
-let pool
-
-const getDBConnection = () => {
-  // if (connection) return connection
-
-  pool = mysql.createPool({
+const getDBPool = () => {
+  return mysql.createPool({
     connectionLimit : 10,
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    database: process.env.DB_DATABASE
   })
-
-  pool.connect((err) => {
-    if (err) {
-      console.error('Connection unsuccessful', err)
-    } else {
-      console.log('Successful connect')
-    }
-  })
-
-  return pool
 }
-
-module.exports = getDBConnection()
+module.exports = getDBPool();
